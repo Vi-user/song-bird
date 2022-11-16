6e521777a9b200d0eb60d7be87897646656b268e
Vi-user <vi.volkova.2712@gmail.com>
2022-11-17 00:25:59 +0300

    feat: animation on start page

--- songbird/src/js/index.js
+++ songbird/src/js/index.js
@@ -1,5 +1,7 @@
 console.log('main page')
 const paragraphList = document.querySelectorAll('.main-paragraph')
+const container = document.querySelector('.main-index-block')
+const startQuizBtn = document.querySelector('.start-quiz__btn')
 
 
 for (let i = 0; i < paragraphList.length; i++) {
@@ -7,3 +9,35 @@ for (let i = 0; i < paragraphList.length; i++) {
     paragraphList[i].classList.add('show-paragraph')
   } , i*1000)
 }
+
+function getRandomSize(min, max) {
+  return Math.round(Math.random() * (max - min) + min)
+}
+
+function createSymbol() {
+  const newEl = document.createElement('div');
+  newEl.textContent = '?';
+  const size = getRandomSize(10, 100)
+  const incline = getRandomSize(0, 360)
+  const {width, height} = container.getBoundingClientRect()
+  const x = getRandomSize(0, width-size)
+  const y = getRandomSize(0, height-size)
+
+  newEl.classList.add('symbol-question');
+  newEl.style.fontSize = `${size}px`
+  newEl.style.transform  = `rotate(${incline}deg)`
+  newEl.style.top = `${y}px`
+  newEl.style.left = `${x}px`
+  setTimeout(() => {
+    newEl.remove()
+  }, 1500)
+  container.append(newEl)
+}
+
+setInterval(() => {
+  createSymbol()
+}, 600);
+
+startQuizBtn.addEventListener('click', () => {
+  window.location.href = './src/pages/quiz.html'
+})
